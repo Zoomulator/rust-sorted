f5958371098d55b5da3c9c3b9931b6b67e73bd01
Kim Simmons <zoomulator@gmail.com>
2017-06-06 18:28:16 +0200

    No need for SortedSlice and SortedVec any more. Sorted<T> does it all.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 pub mod macro_sort_order;
 
 mod sorted_iter;
-mod sorted_slice;
-mod sorted_vec;
 mod trait_is_sorted;
 mod trait_sort_order;
 mod trait_sortable;
@@ -11,8 +9,6 @@ mod trait_sorted_insert;
 mod type_sorted;
 
 pub use sorted_iter::SortedIter;
-pub use sorted_slice::SortedSlice;
-pub use sorted_vec::SortedVec;
 pub use trait_is_sorted::IsSorted;
 pub use trait_sort_order::{SortOrder, AscendingOrder, DescendingOrder};
 pub use trait_sortable::Sortable;
