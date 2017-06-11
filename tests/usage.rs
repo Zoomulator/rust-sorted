601ec4e025af0f3f4018f40c6c5cf8123ce2b377
Kim Simmons <zoomulator@gmail.com>
2017-06-11 16:00:56 +0200

    Adding some clarifying comments to the usage tests.

--- tests/usage.rs
+++ tests/usage.rs
@@ -14,24 +14,28 @@ order_by_key!{ KeySecondOrder:
 #[test]
 fn sorted_array() {
     let arr = [7, 2, 9, 6];
+    // Sort the array, resulting in a Sorted<[T;n], AscendingOrder>.
     let v = AscendingOrder::by_sorting(arr);
-    assert_eq!(*v.as_inner(), [2, 6, 7, 9]);
+    // Retrieve a reference to the inner type with .as_inner.
+    assert_eq!(v.as_inner(), &[2, 6, 7, 9]);
 }
 
 #[test]
 fn ascending_order() {
     let arr = AscendingOrder::by_sorting([3, 4, 1, 2]);
-    assert_eq!(*arr.as_inner(), [1, 2, 3, 4]);
+    assert_eq!(arr.as_inner(), &[1, 2, 3, 4]);
 }
 
 #[test]
 fn descending_order() {
     let arr = DescendingOrder::by_sorting([1, 3, 4, 2]);
-    assert_eq!(*arr.as_inner(), [4, 3, 2, 1]);
+    assert_eq!(arr.as_inner(), &[4, 3, 2, 1]);
 }
 
 #[test]
 fn sorted_slice() {
+    // You can also refer to a slice that should be sorted.
+    // This leaves the ownership outside of the Sorted<> type.
     let mut arr = [3, 2, 4, 1];
     let s = AscendingOrder::by_sorting(&mut arr[..]);
     assert_eq!(s.as_inner(), &[1, 2, 3, 4]);
@@ -45,6 +49,8 @@ fn sorted_vec() {
 
 #[test]
 fn sort_by_first() {
+    // It's easy to sort by a tuple key. Specifiy which key and in what order
+    // it should be sorted.
     let s = vec![(5, 3), (2, 7), (3, 4)];
     let v = KeyOrder::<keys::Key0, AscendingOrder>::by_sorting(s);
     assert_eq!(&[(2, 7), (3, 4), (5, 3)], v.as_slice());
@@ -64,13 +70,18 @@ fn sort_with_property_key_type() {
     impl Record {
         pub fn new(n: u32) -> Self { Record{ stuff: 0, id: n} }
     }
+
+    // Sorting via an arbitrary property takes a bit more work.
+    // First define a tag type to identify what property we want to sort by.
     #[derive(Debug, Clone, Copy)]
     struct IdKey;
+    // Then implement how to get that key from our Record type.
     impl Key<Record> for IdKey {
         type Key = u32;
         fn key(r: &Record) -> Self::Key { r.id }
     }
 
+    // You can now sort the Records by a property in any order.
     let v = KeyOrder::<IdKey, AscendingOrder>::by_sorting(
         vec![Record::new(2), Record::new(3), Record::new(1)]
     );
@@ -83,6 +94,8 @@ fn sort_with_property_key_type() {
 #[test]
 fn sort_by_property_string() {
     use std::cmp::Ordering;
+    // Using strings as keys has some lifetime issues with the previous key
+    // pattern. It's however possible, just not as flexible.
     #[derive(Debug, Clone, PartialEq)]
     struct Person { name: String, x: i32 };
     impl Person {
@@ -90,13 +103,20 @@ fn sort_by_property_string() {
             Self { name: n.to_string(), x: 0 }
         }
     }
+
+    // We'll implement our own SortOrder type. Starting with a tag type.
     #[derive(Debug, Clone, Copy)]
     struct OrderByName;
+    // Then implement the SortOrder trait, per struct supporting it.
     impl SortOrder<Person> for OrderByName {
         fn cmp(a: &Person, b: &Person) -> Ordering {
             a.name.cmp(&b.name)
         }
     }
+
+    // It will only provide one ordering for what you define, so if you need to
+    // support multiple ways of ordering by name it will result in a bit of
+    // boilerplate.
     let v = OrderByName::by_sorting(
         vec![Person::new("Bob"), Person::new("Cecil"), Person::new("Alice")]
     );
@@ -108,6 +128,9 @@ fn sort_by_property_string() {
 
 #[test]
 fn sorted_slice_from_sorted_vec() {
+    // The as_ref works like Option::as_ref; it returns a Sorted type with the
+    // inner type being a reference to the original. If the inner type implements
+    // the correct AsRef, it'll work.
     fn take_sorted_slice<'a>(slice: Sorted<'a, &'a [i32], AscendingOrder>) {
         assert_eq!(&[1, 2, 4, 9, 33][..], *slice);
     }
@@ -115,12 +138,13 @@ fn sorted_slice_from_sorted_vec() {
     take_sorted_slice(vec.as_ref());
 }
 
-/// TODO: Can you get a Sorted<'a, &vec, O> from Sorted::as_ref?
 #[test]
-fn uhm() {
+fn sorted_vec_ref() {
+    fn take_sorted_vec<'a>(refvec: Sorted<'a, &'a Vec<i32>, AscendingOrder>) {
+        assert_eq!(&[1,2,3,4], refvec.as_slice())
+    }
     let vec = AscendingOrder::by_sorting(vec![3,2,4,1]);
-    let rvec: Sorted<&Vec<i32>, AscendingOrder> = vec.as_ref();
-    rvec.as_slice();
+    take_sorted_vec(vec.as_ref());
 }
 
 #[test]
@@ -129,12 +153,14 @@ fn sorted_vec_from_sorted_slice() {
     type SortedVec<'a, T, O> = Sorted<'a, Vec<T>, O>;
     let mut arr = [5, 3, 7, 9];
     let slice = AscendingOrder::by_sorting(&mut arr[..]);
+    // This is currently unstable as it doesn't seem possible to guarantee sortedness.
     let vec = SortedVec::from(slice);
     assert_eq!([3, 5, 7, 9], vec.as_slice());
 }
 
 #[test]
 fn take_sorted_iterator() {
+    // Sorted types can generate IsSorted iterators.
     fn take_sorted<I>(sorted: I)
         where I: IntoIterator<Item = i32> + IsSorted
     {
@@ -147,6 +173,8 @@ fn take_sorted_iterator() {
 
 #[test]
 fn take_sorted_ref_iterator() {
+    // By-ref iterators can only be created via Sorted::iter() right now.
+    // I.e there is no IntoIterator for &Sorted<>.
     fn take_sorted_ref<'a,I>(sorted: I)
         where I: IntoIterator<Item = &'a i32> + IsSorted
     {
@@ -159,6 +187,8 @@ fn take_sorted_ref_iterator() {
 
 #[test]
 fn sorted_insert() {
+    // The Sorted type only provides mutable operations that keep the collection
+    // sorted.
     let mut vec = AscendingOrder::by_sorting(vec![4, 8, 2, 0]);
     vec.insert(6);
     assert_eq!([0, 2, 4, 6, 8], vec.as_slice());
@@ -166,6 +196,7 @@ fn sorted_insert() {
 
 #[test]
 fn sorted_vec_from_sorted_iterator() {
+    // You can create Sorted collections from Sorted iterators.
     type SortedVec<'a, T, O> = Sorted<'a, Vec<T>, O>;
     let v0 = AscendingOrder::by_sorting(vec![3,1,4,2]);
     let it = v0.into_iter();
